d7afc213a3e90345ec1316f14f6bafa449886fc4
mbaibatyr <murat_b@mail.ru>
2024-06-14 21:46:11 +0500

    -

---
+++ Client/Form1.Designer.cs
@@ -0,0 +1,142 @@
+﻿namespace Client
+{
+    partial class Form1
+    {
+        /// <summary>
+        ///  Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        ///  Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        ///  Required method for Designer support - do not modify
+        ///  the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            button1 = new Button();
+            label2 = new Label();
+            tbPort = new TextBox();
+            label1 = new Label();
+            tbHost = new TextBox();
+            listBox1 = new ListBox();
+            label3 = new Label();
+            tbMessage = new TextBox();
+            SuspendLayout();
+            // 
+            // button1
+            // 
+            button1.Location = new Point(25, 374);
+            button1.Name = "button1";
+            button1.Size = new Size(95, 23);
+            button1.TabIndex = 11;
+            button1.Text = "SendToServer";
+            button1.UseVisualStyleBackColor = true;
+            button1.Click += button1_Click;
+            // 
+            // label2
+            // 
+            label2.AutoSize = true;
+            label2.Location = new Point(138, 413);
+            label2.Name = "label2";
+            label2.Size = new Size(35, 15);
+            label2.TabIndex = 10;
+            label2.Text = "PORT";
+            // 
+            // tbPort
+            // 
+            tbPort.Location = new Point(182, 410);
+            tbPort.Name = "tbPort";
+            tbPort.Size = new Size(115, 23);
+            tbPort.TabIndex = 9;
+            tbPort.Text = "8090";
+            // 
+            // label1
+            // 
+            label1.AutoSize = true;
+            label1.Location = new Point(138, 374);
+            label1.Name = "label1";
+            label1.Size = new Size(37, 15);
+            label1.TabIndex = 8;
+            label1.Text = "HOST";
+            // 
+            // tbHost
+            // 
+            tbHost.Location = new Point(182, 371);
+            tbHost.Name = "tbHost";
+            tbHost.Size = new Size(115, 23);
+            tbHost.TabIndex = 7;
+            tbHost.Text = "localhost";
+            // 
+            // listBox1
+            // 
+            listBox1.Dock = DockStyle.Top;
+            listBox1.FormattingEnabled = true;
+            listBox1.ItemHeight = 15;
+            listBox1.Location = new Point(0, 0);
+            listBox1.Name = "listBox1";
+            listBox1.Size = new Size(531, 349);
+            listBox1.TabIndex = 6;
+            // 
+            // label3
+            // 
+            label3.AutoSize = true;
+            label3.Location = new Point(138, 452);
+            label3.Name = "label3";
+            label3.Size = new Size(53, 15);
+            label3.TabIndex = 13;
+            label3.Text = "Message";
+            // 
+            // tbMessage
+            // 
+            tbMessage.Location = new Point(197, 449);
+            tbMessage.Name = "tbMessage";
+            tbMessage.Size = new Size(115, 23);
+            tbMessage.TabIndex = 12;
+            tbMessage.Text = "Hello STEP";
+            // 
+            // Form1
+            // 
+            AutoScaleDimensions = new SizeF(7F, 15F);
+            AutoScaleMode = AutoScaleMode.Font;
+            ClientSize = new Size(531, 507);
+            Controls.Add(label3);
+            Controls.Add(tbMessage);
+            Controls.Add(button1);
+            Controls.Add(label2);
+            Controls.Add(tbPort);
+            Controls.Add(label1);
+            Controls.Add(tbHost);
+            Controls.Add(listBox1);
+            Name = "Form1";
+            Text = "Form1";
+            ResumeLayout(false);
+            PerformLayout();
+        }
+
+        #endregion
+
+        private Button button1;
+        private Label label2;
+        private TextBox tbPort;
+        private Label label1;
+        private TextBox tbHost;
+        private ListBox listBox1;
+        private Label label3;
+        private TextBox tbMessage;
+    }
+}
--- Server/Form1.cs
+++ Server/Form1.cs
@@ -16,31 +16,35 @@ namespace Server
             StartServer();
         }
 
-        void StartServer()
+        async Task StartServer()
         {
-            var ip = Dns.GetHostEntry(tbHost.Text);
-            var ad = ip.AddressList[0];
-            var ep = new IPEndPoint(ad, int.Parse(tbPort.Text));
-            Socket listener = new Socket(ad.AddressFamily, SocketType.Stream, ProtocolType.Tcp);
-            listener.Bind(ep);
-            listener.Listen(20);
-            listBox1.Items.Add("Слушаем " + ep);
-            Socket handler;
-            while (true) 
+            await Task.Run(() =>
             {
-                byte[] bytes = new byte[1024];
-                handler = listener.Accept();
-                string data = null;
-                int count = handler.Receive(bytes);
-                var st = Encoding.UTF8.GetString(bytes, 0, count);
-                listBox1.Items.Add(st);
-                data += Encoding.UTF8.GetString(bytes, 0, count);
-                string replay = "Спасибо за " + data;
-                byte[] response = Encoding.UTF8.GetBytes(replay);
-                handler.Send(response);
-                handler.Shutdown(SocketShutdown.Both);
-                handler.Close();
-            }
+
+                var ip = Dns.GetHostEntry(tbHost.Text);
+                var ad = ip.AddressList[0];
+                var ep = new IPEndPoint(ad, int.Parse(tbPort.Text));
+                Socket listener = new Socket(ad.AddressFamily, SocketType.Stream, ProtocolType.Tcp);
+                listener.Bind(ep);
+                listener.Listen(20);
+                listBox1.Items.Add("Слушаем " + ep);
+                Socket handler;
+                while (true)
+                {
+                    byte[] bytes = new byte[1024];
+                    handler = listener.Accept();
+                    string data = null;
+                    int count = handler.Receive(bytes);
+                    var st = Encoding.UTF8.GetString(bytes, 0, count);
+                    listBox1.Items.Add(st);
+                    data += Encoding.UTF8.GetString(bytes, 0, count);
+                    string replay = "Спасибо за " + data;
+                    byte[] response = Encoding.UTF8.GetBytes(replay);
+                    handler.Send(response);
+                    handler.Shutdown(SocketShutdown.Both);
+                    handler.Close();
+                }
+            })
         }
     }
 }
