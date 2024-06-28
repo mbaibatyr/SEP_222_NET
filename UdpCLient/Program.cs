a103c5fc78688181ffee509fd3d087d1b7981c00
mbaibatyr <murat_b@mail.ru>
2024-06-28 21:39:02 +0500

    --

---
+++ UdpCLient/Program.cs
@@ -0,0 +1,36 @@
+﻿using System.Net.Sockets;
+using System.Net;
+using System.Text;
+
+namespace UdpCLient
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            UDP.Send();
+        }
+    }
+
+    class UDP
+    {
+        public static void Send()
+        {
+            UdpClient Client = new UdpClient();
+            var data = Encoding.UTF8.GetBytes("hello step");
+            Client.Send(data, data.Length, GetLocalIPAddress(), 8001);
+        }
+        public static string GetLocalIPAddress()
+        {
+            var host = Dns.GetHostEntry(Dns.GetHostName());
+            foreach (var ip in host.AddressList)
+            {
+                if (ip.AddressFamily == AddressFamily.InterNetwork)
+                {
+                    return ip.ToString();
+                }
+            }
+            return null;
+        }
+    }
+}
---
+++ UdpServer/Program.cs
@@ -0,0 +1,48 @@
+﻿using System.Net.Sockets;
+using System.Net;
+using System.Text;
+
+namespace UdpServer
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            //Console.WriteLine(UDP.GetLocalIPAddress());
+            UDP.Listen();
+        }
+    }
+
+    class UDP
+    {
+        public static void Listen()
+        {
+            int PORT = 8001;
+            UdpClient udpClient = new UdpClient();
+            udpClient.Client.Bind(new IPEndPoint(IPAddress.Any, PORT));
+            Console.WriteLine("Listening...");
+            var from = new IPEndPoint(0, 0);
+            var task = Task.Run(() =>
+            {
+                while (true)
+                {
+                    var recvBuffer = udpClient.Receive(ref from);
+                    Console.WriteLine(Encoding.UTF8.GetString(recvBuffer));
+                }
+            });
+            task.Wait();
+        }
+        public static string GetLocalIPAddress()
+        {
+            var host = Dns.GetHostEntry(Dns.GetHostName());
+            foreach (var ip in host.AddressList)
+            {
+                if (ip.AddressFamily == AddressFamily.InterNetwork)
+                {
+                    return ip.ToString();
+                }
+            }
+            return null;
+        }
+    }
+}
