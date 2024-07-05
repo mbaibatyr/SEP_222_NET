1b2211efac94ff7aa36543a355afcbba45cd2624
mbaibatyr <murat_b@mail.ru>
2024-07-05 20:17:49 +0500

    --

---
+++ MyHTTP/Program.cs
@@ -0,0 +1,45 @@
+﻿using System.Net;
+using System.Text;
+
+namespace MyHTTP
+{
+    internal class Program
+    {
+        static Thread threadListener;
+        static void Main(string[] args)
+        {
+            threadListener = new Thread(new ParameterizedThreadStart(Start));
+            threadListener.Start("http://localhost:12345/");
+
+        }
+
+        public static void Start(object prefix)
+        {
+            HttpListener listener = new HttpListener();
+            listener.Prefixes.Add(prefix.ToString());
+            listener.Start();
+            while (true)
+            {
+                Console.WriteLine("Прослушка работает..");
+                HttpListenerContext context = listener.GetContext();
+
+                HttpListenerRequest request = context.Request;
+                var qry = request.RawUrl.Split('/');
+                string Segment = null;
+                Segment = qry[1];
+                Console.WriteLine("Получено.." + Segment);
+                HttpListenerResponse response = context.Response;
+
+                string responseString = "<HTML><BODY> HELLO " + Segment + " </BODY></HTML>";
+                byte[] buffer = Encoding.UTF8.GetBytes(responseString);
+
+                response.ContentLength64 = buffer.Length;
+                Stream output = response.OutputStream;
+                output.Write(buffer, 0, buffer.Length);
+
+                output.Close();
+            }
+            //listener.Stop();
+        }
+    }
+}
