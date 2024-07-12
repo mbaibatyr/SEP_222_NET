3af6ea74b836710ad5ac901b8031bb5ae3a391d4
mbaibatyr <murat_b@mail.ru>
2024-07-12 20:46:33 +0500

    -

---
+++ HTTP_Client/Program.cs
@@ -0,0 +1,38 @@
+﻿namespace HTTP_Client
+{
+    internal class Program
+    {
+        static async Task Main(string[] args)
+        {
+            await Method_2();
+        }
+
+        static async Task<string> Method_1()
+        {
+            using (HttpClient client = new HttpClient())
+            {
+                var result = await client.GetStringAsync("https://www.google.com/");
+                return result;
+            }
+        }
+
+        static async Task Method_2()
+        {
+            using (HttpClient client = new HttpClient())
+            {
+                var result = await client.GetAsync("https://www.google.com/");
+
+                //    //var result = response.Content.ReadAsStringAsync().Result;
+                using (StreamReader sr = new StreamReader(await result.Content.ReadAsStreamAsync()))
+                {
+                    string result2 = sr.ReadToEnd();
+                    Console.WriteLine(result2);
+                }
+
+            }
+        }
+
+
+
+    }
+}
