4a23974a0eb1f0e780082b5bcc545c6444a0e85b
mbaibatyr <murat_b@mail.ru>
2024-07-19 20:28:14 +0500

    --

---
+++ MyFTP/Program.cs
@@ -0,0 +1,28 @@
+﻿using FluentFTP;
+using System.Net;
+using static System.Net.WebRequestMethods;
+
+namespace MyFTP
+{
+    internal class Program
+    {
+        //https://filezilla.ru/download/FileZillaPortable_3.63.2.paf.exe
+        static string url = "ftp://ftp.dlptest.com/";
+        static string user = "dlpuser";
+        static string password = "rNrKYTX9g7z3RgJRmxWuGHbeu";
+
+        static void FTP_test()
+        {
+            FtpClient client = new FtpClient();
+            client.Host = url;
+            client.Credentials = new NetworkCredential(user, password);
+            client.Connect();
+            var status = client.UploadFile("123.txt", "/07/19/123kdadkadakdaskdjasklj.txt");
+            ;
+        }
+        static void Main(string[] args)
+        {
+            FTP_test();
+        }
+    }
+}
